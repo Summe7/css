90342e854e5b7fb45ef8493135eb280b9ce3fd66
Summe7 <soxroot@gmail.com>
2024-08-29 19:51:24 +0800

    Update data.ts

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -10,6 +10,41 @@ export const NAV_DATA: NavData[] = [
     title: '常用工具',
     items: [
       {
+        icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcK5Zg12Cc0JIzfuGz02bhVl1VUVRg4lrPs6Hn9cE22P5rs1bv5K5D4WgATA&s',
+        title: 'cloudflare',
+        desc: '前端 API 兼容性查询',
+        link: 'https://dash.cloudflare.com/',
+      },
+      {
+        icon: 'https://tinypng.com/images/apple-touch-icon.png',
+        title: 'TinyPNG',
+        desc: '在线图片压缩工具',
+        link: 'https://tinypng.com',
+      },
+      {
+        icon: 'https://devtool.tech/logo.svg',
+        title: '开发者武器库',
+        desc: '开发者武器库，做开发者最专业最好用的专业工具箱',
+        link: 'https://devtool.tech',
+      },
+      {
+        icon: 'https://tool.lu/favicon.ico',
+        title: '在线工具',
+        desc: '开发人员的工具箱',
+        link: 'https://tool.lu',
+      },
+      {
+        icon: '/icons/json-cn.ico',
+        title: 'Json 中文网',
+        desc: 'JSON 在线解析及格式化验证',
+        link: 'https://www.json.cn',
+      },
+    ],
+  },
+  {
+    title: '搜索引擎',
+    items: [
+      {
         icon: 'https://caniuse.com/img/favicon-128.png',
         title: 'Can I use',
         desc: '前端 API 兼容性查询',
